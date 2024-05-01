70bd5990c2808fc1187d77b29169666bcb0097b0
Egor <Egorkrintchik@gmail.com>
2024-05-01 20:48:50 +0300

    Сделал тему 10

---
+++ Theme10/Task1/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+
+    class Notebook
+    {
+        private string name;
+        private int pageCount;
+
+        public Notebook(string name, int pageCount)
+        {
+            this.name = name;
+            this.pageCount = pageCount;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public int GetPageCount()
+        {
+            return pageCount;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Тетрадь: {name}, Количество листов: {pageCount}");
+        }
+
+        public virtual double CalculateCost()
+        {
+            return 15 * pageCount;
+        }
+    }
+
+    class EnhancedNotebook : Notebook
+    {
+        private string color;
+
+        public EnhancedNotebook(string name, int pageCount, string color) : base(name, pageCount)
+        {
+            this.color = color;
+        }
+
+        public new void DisplayInfo()
+        {
+            base.DisplayInfo();
+            Console.WriteLine($"Цвет: {color}");
+        }
+
+        public override double CalculateCost()
+        {
+            return base.CalculateCost() + 5;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Notebook basicNotebook = new Notebook("Простая", 50);
+            Console.WriteLine("Базовая тетрадь:");
+            basicNotebook.DisplayInfo();
+            Console.WriteLine($"Стоимость: {basicNotebook.CalculateCost()}");
+
+            Console.WriteLine();
+
+            EnhancedNotebook enhancedNotebook = new EnhancedNotebook("Цветная", 80, "Синий");
+            Console.WriteLine("Улучшенная тетрадь:");
+            enhancedNotebook.DisplayInfo();
+            Console.WriteLine($"Стоимость: {enhancedNotebook.CalculateCost()}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Theme10/Task2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class A
+    {
+        private int a = 5;
+        private int b = 10;
+
+        public int C
+        {
+            get { return a + b; }
+        }
+    }
+
+    class B : A
+    {
+        private int d = 7;
+
+        public B() { }
+
+        public B(int d)
+        {
+            this.d = d;
+        }
+
+        public int D
+        {
+            get { return d; }
+            set { d = value; }
+        }
+
+        public int C2
+        {
+            get
+            {
+                int result = 0;
+                int i = 0;
+                while (i < 5)
+                {
+                    result += C + D;
+                    i++;
+                }
+                return result;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A objA = new A();
+            B objB1 = new B();
+            B objB2 = new B(3);
+
+            Console.WriteLine($"Значение свойства C у объекта класса A: {objA.C}");
+            Console.WriteLine($"Значение свойства C2 у объекта класса B (собственный конструктор): {objB1.C2}");
+            Console.WriteLine($"Значение свойства C2 у объекта класса B (наследуемый конструктор): {objB2.C2}");
+
+            Console.ReadKey();
+        }
+    }
+}
