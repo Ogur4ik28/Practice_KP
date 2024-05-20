3eace6e41e1847abd88fd7fe7cea6db8d2565283
Egor <Egorkrintchik@gmail.com>
2024-05-20 22:08:24 +0300

    Сделал тему 21

---
+++ Theme21/Task1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class Singleton
+    {
+        private static Singleton instance = null;
+
+        private static readonly object lockObject = new object();
+
+        private Singleton()
+        {
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (lockObject)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new Singleton();
+                        }
+                    }
+                }
+                return instance;
+            }
+        }
+
+        public void ShowMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton singleton1 = Singleton.Instance;
+            Singleton singleton2 = Singleton.Instance;
+
+            if (singleton1 == singleton2)
+            {
+                Console.WriteLine("Оба переменных ссылаются на один и тот же экземпляр.");
+            }
+
+            singleton1.ShowMessage("Hello, Singleton Pattern!");
+
+            Console.ReadKey();
+        }
+    }
+}
