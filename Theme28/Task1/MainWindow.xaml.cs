dd10d87ba70516b7ef3a4e01ae0e6da4b79aba74
Egor <Egorkrintchik@gmail.com>
2024-05-27 16:52:14 +0300

    Сделал тему 28

---
+++ Theme28/Task1/MainWindow.xaml.cs
@@ -0,0 +1,143 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Task1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            DrawGraphics(drawingCanvas);
+        }
+
+        private void DrawGraphics(Canvas canvas)
+        {
+            // Нарисовать треугольник
+            var triangle = CreatePolygon(new PointCollection
+            {
+                new Point(50, 0),
+                new Point(0, 100),
+                new Point(100, 100)
+            }, Brushes.Black, 10, 10);
+            canvas.Children.Add(triangle);
+
+            // Нарисовать эллипс
+            var ellipse = CreateEllipse(100, 50, Brushes.Black, null, 120, 10);
+            canvas.Children.Add(ellipse);
+
+            // Нарисовать закрашенный круг
+            var filledCircle = CreateEllipse(100, 100, null, Brushes.Blue, 200, 10);
+            canvas.Children.Add(filledCircle);
+
+            // Нарисовать закрашенный прямоугольник
+            var filledRectangle = CreateRectangle(100, 50, null, Brushes.Green, 320, 10);
+            canvas.Children.Add(filledRectangle);
+
+            // Нарисовать сектор
+            var sector = CreateSector(new Point(100, 100), new Point(200, 100), new Point(150, 50), 50, Brushes.Red, 400, 10);
+            canvas.Children.Add(sector);
+
+            // Нарисовать три концентрических круга
+            DrawConcentricCircles(canvas, 3, 50, 50, 200);
+
+            // Нарисовать серию из семи квадратов
+            DrawSeriesOfSquares(canvas, 7, 30, 50, 2, 150, 10, 400);
+
+            // Нарисовать шахматную доску
+            DrawCheckerboard(canvas, 8, 20, 20, 300, 10, 200);
+        }
+
+        private Polygon CreatePolygon(PointCollection points, Brush stroke, double top, double left)
+        {
+            var polygon = new Polygon
+            {
+                Points = points,
+                Stroke = stroke
+            };
+            Canvas.SetTop(polygon, top);
+            Canvas.SetLeft(polygon, left);
+            return polygon;
+        }
+
+        private Ellipse CreateEllipse(double width, double height, Brush stroke, Brush fill, double top, double left)
+        {
+            var ellipse = new Ellipse
+            {
+                Width = width,
+                Height = height,
+                Stroke = stroke,
+                Fill = fill
+            };
+            Canvas.SetTop(ellipse, top);
+            Canvas.SetLeft(ellipse, left);
+            return ellipse;
+        }
+
+        private Rectangle CreateRectangle(double width, double height, Brush stroke, Brush fill, double top, double left)
+        {
+            var rectangle = new Rectangle
+            {
+                Width = width,
+                Height = height,
+                Stroke = stroke,
+                Fill = fill
+            };
+            Canvas.SetTop(rectangle, top);
+            Canvas.SetLeft(rectangle, left);
+            return rectangle;
+        }
+
+        private Path CreateSector(Point startPoint, Point lineEndPoint, Point arcPoint, double size, Brush fill, double top, double left)
+        {
+            var path = new Path { Fill = fill };
+            var figure = new PathFigure { StartPoint = startPoint };
+            figure.Segments.Add(new LineSegment(lineEndPoint, true));
+            figure.Segments.Add(new ArcSegment(arcPoint, new Size(size, size), 0, false, SweepDirection.Clockwise, true));
+            figure.Segments.Add(new LineSegment(startPoint, true));
+            path.Data = new PathGeometry(new[] { figure });
+            Canvas.SetTop(path, top);
+            Canvas.SetLeft(path, left);
+            return path;
+        }
+
+        private void DrawConcentricCircles(Canvas canvas, int count, double centerX, double centerY, double offsetX)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                var circle = CreateEllipse(100 + i * 20, 100 + i * 20, Brushes.Black, null, centerY - i * 10, centerX - i * 10 + offsetX);
+                circle.StrokeThickness = 2;
+                canvas.Children.Add(circle);
+            }
+        }
+
+        private void DrawSeriesOfSquares(Canvas canvas, int count, double width, double height, double strokeThickness, double topOffset, double leftOffset, double offsetX)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                var square = CreateRectangle(width, height, Brushes.Black, null, topOffset + i * 35, leftOffset + i * 15 + offsetX);
+                square.StrokeThickness = strokeThickness;
+                canvas.Children.Add(square);
+            }
+        }
+
+        private void DrawCheckerboard(Canvas canvas, int size, double squareWidth, double squareHeight, double topOffset, double leftOffset, double offsetX)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    var square = CreateRectangle(squareWidth, squareHeight, null, (i + j) % 2 == 0 ? Brushes.Black : Brushes.White, topOffset + i * squareHeight, leftOffset + j * squareWidth + offsetX);
+                    canvas.Children.Add(square);
+                }
+            }
+        }
+    }
+}
---
+++ Theme28/Task2/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Task2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            DrawGraphics(e.Graphics);
+        }
+
+        private void DrawGraphics(Graphics g)
+        {
+            // Рисуем треугольник
+            Point[] trianglePoints = { new Point(50, 0), new Point(0, 100), new Point(100, 100) };
+            g.DrawPolygon(Pens.Black, trianglePoints);
+
+            // Рисуем эллипс
+            g.DrawEllipse(Pens.Black, new Rectangle(120, 120, 100, 50));
+
+            // Рисуем закрашенный круг
+            g.FillEllipse(Brushes.Blue, new Rectangle(240, 240, 100, 100));
+
+            // Рисуем закрашенный прямоугольник
+            g.FillRectangle(Brushes.Green, new Rectangle(360, 360, 100, 50));
+
+            // Рисуем сектор
+            g.FillPie(Brushes.Red, new Rectangle(480, 480, 100, 100), 0, 120);
+
+            // Рисуем три концентрических круга
+            DrawConcentricCircles(g, 3, new Point(400, 150), 40);
+
+            // Рисуем серию из семи квадратов
+            DrawSeriesOfSquares(g, 7, new Point(200, 300), 30, 20);
+
+            // Рисуем шахматную доску
+            DrawCheckerboard(g, new Point(400, 400), 8, 20);
+        }
+
+        private void DrawConcentricCircles(Graphics g, int count, Point center, int initialRadius)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                int radius = initialRadius + i * 20;
+                g.DrawEllipse(Pens.Black, new Rectangle(center.X - radius, center.Y - radius, 2 * radius, 2 * radius));
+            }
+        }
+
+        private void DrawSeriesOfSquares(Graphics g, int count, Point startPoint, int size, int offset)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                g.DrawRectangle(Pens.Black, new Rectangle(startPoint.X + i * offset, startPoint.Y + i * offset, size, size));
+            }
+        }
+
+        private void DrawCheckerboard(Graphics g, Point startPoint, int gridSize, int cellSize)
+        {
+            for (int i = 0; i < gridSize; i++)
+            {
+                for (int j = 0; j < gridSize; j++)
+                {
+                    Brush brush = (i + j) % 2 == 0 ? Brushes.Black : Brushes.White;
+                    g.FillRectangle(brush, new Rectangle(startPoint.X + j * cellSize, startPoint.Y + i * cellSize, cellSize, cellSize));
+                }
+            }
+        }
+    }
+}
