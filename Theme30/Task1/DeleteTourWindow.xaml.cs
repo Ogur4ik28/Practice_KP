8c0ce23312da24b28317d128e19eb159a012950c
Egor <Egorkrintchik@gmail.com>
2024-05-27 19:38:04 +0300

    Сделал тему 30

---
+++ Theme30/Task1/DeleteTourWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+
+namespace TravelAgencyApp
+{
+    public partial class RemoveTouristWindow : Window
+    {
+        public int SelectedTourCode { get; private set; }
+
+        public RemoveTouristWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnDeleteButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(tourCodeTextBox.Text, out int tourId))
+            {
+                SelectedTourCode = tourId;
+                DialogResult = true;
+            }
+            else
+            {
+                MessageBox.Show("Введите корректный код тура.");
+            }
+        }
+
+        private void OnCancelButtonClick(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+    }
+}
---
+++ Theme30/Task1/MainWindow.xaml.cs
@@ -0,0 +1,117 @@
+﻿using System.Data;
+using System.Windows;
+using System.Data.SQLite;
+
+namespace TravelAgencyApp
+{
+    public partial class MainWindow : Window
+    {
+        private readonly string dbConnection = @"DataSource=../../../Database/DBTur_firm.db;Version=3;";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DisplayAllTourists();
+        }
+
+        private void OnSelectAllToursClick(object sender, RoutedEventArgs e)
+        {
+            DisplayAllTours();
+        }
+
+        private void DisplayAllTours()
+        {
+            using (SQLiteConnection connection = new(dbConnection))
+            {
+                connection.Open();
+                string query = "SELECT * FROM Туры";
+                SQLiteDataAdapter adapter = new(query, connection);
+                DataTable toursTable = new();
+                adapter.Fill(toursTable);
+                dataGridTours.ItemsSource = toursTable.DefaultView;
+            }
+        }
+
+        private void DisplayAllTourists()
+        {
+            using (SQLiteConnection connection = new(dbConnection))
+            {
+                connection.Open();
+                string query = "SELECT * FROM Туристы";
+                SQLiteDataAdapter adapter = new(query, connection);
+                DataTable touristsTable = new();
+                adapter.Fill(touristsTable);
+                dataGridTourists.ItemsSource = touristsTable.DefaultView;
+            }
+        }
+
+        private void OnDeleteTourClick(object sender, RoutedEventArgs e)
+        {
+            DeleteTouristWindow deleteWindow = new();
+            if (deleteWindow.ShowDialog() == true)
+            {
+                int tourId = deleteWindow.TourCode;
+                using (SQLiteConnection connection = new(dbConnection))
+                {
+                    connection.Open();
+                    string query = "DELETE FROM Туры WHERE Код_тура = @Код_тура";
+                    SQLiteCommand command = new(query, connection);
+                    command.Parameters.AddWithValue("@Код_тура", tourId);
+                    int rowsAffected = command.ExecuteNonQuery();
+                    MessageBox.Show(rowsAffected > 0 ? "Тур удален." : "Тур с таким кодом не найден.");
+                }
+                DisplayAllTours();
+            }
+        }
+
+        private void OnAddTouristClick(object sender, RoutedEventArgs e)
+        {
+            AddTouristWindow addWindow = new();
+            if (addWindow.ShowDialog() == true)
+            {
+                string firstName = addWindow.Name;
+                string lastName = addWindow.Surname;
+                string middleName = addWindow.MiddleName;
+
+                using (SQLiteConnection connection = new(dbConnection))
+                {
+                    connection.Open();
+                    string query = "INSERT INTO Туристы (Фамилия, Имя, Отчество) VALUES (@Фамилия, @Имя, @Отчество)";
+                    SQLiteCommand command = new(query, connection);
+                    command.Parameters.AddWithValue("@Фамилия", lastName);
+                    command.Parameters.AddWithValue("@Имя", firstName);
+                    command.Parameters.AddWithValue("@Отчество", middleName);
+                    command.ExecuteNonQuery();
+                }
+                MessageBox.Show("Турист добавлен.");
+                DisplayAllTourists();
+            }
+        }
+
+        private void OnUpdateTouristClick(object sender, RoutedEventArgs e)
+        {
+            UpdateTouristWindow updateWindow = new();
+            if (updateWindow.ShowDialog() == true)
+            {
+                int touristId = updateWindow.TouristCode;
+                string firstName = updateWindow.Name;
+                string lastName = updateWindow.Surname;
+                string middleName = updateWindow.MiddleName;
+
+                using (SQLiteConnection connection = new(dbConnection))
+                {
+                    connection.Open();
+                    string query = "UPDATE Туристы SET Фамилия = @Фамилия, Имя = @Имя, Отчество = @Отчество WHERE Код_туриста = @Код_туриста";
+                    SQLiteCommand command = new(query, connection);
+                    command.Parameters.AddWithValue("@Код_туриста", touristId);
+                    command.Parameters.AddWithValue("@Фамилия", lastName);
+                    command.Parameters.AddWithValue("@Имя", firstName);
+                    command.Parameters.AddWithValue("@Отчество", middleName);
+                    int rowsAffected = command.ExecuteNonQuery();
+                    MessageBox.Show(rowsAffected > 0 ? "Турист обновлен." : "Турист с таким кодом не найден.");
+                }
+                DisplayAllTourists();
+            }
+        }
+    }
+}
